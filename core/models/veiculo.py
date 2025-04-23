6aa3b35e0bd576f1d3b0c1b261ae3e797f5db5c7
Fefrancine <fernanda.francine.f@gmail.com>
2025-04-23 14:39:23 -0300

    fix: update api para veiculos

--- core/models/veiculo.py
+++ core/models/veiculo.py
@@ -6,10 +6,10 @@ from .modelo import Modelo
 
 
 class Veiculo(models.Model):
-    ano = (models.IntegerField(default=0, null=True),)
-    preco = (models.DecimalField(max_digits=10, decimal_places=2, default=0, null=True),)
-    modelo = (models.ForeignKey(Modelo, on_delete=models.PROTECT, related_name='modelos'),)
-    cor = (models.ForeignKey(Cor, on_delete=models.PROTECT, related_name='cores'),)
+    ano = models.IntegerField(default=0, null=True),
+    preco = models.DecimalField(max_digits=10, decimal_places=2, default=0, null=True),
+    modelo = models.ForeignKey(Modelo, on_delete=models.PROTECT, related_name='modelos'),
+    cor = models.ForeignKey(Cor, on_delete=models.PROTECT, related_name='cores'),
     acessorio = models.ManyToManyField(Acessorio, related_name='acessorios')
 
     def __str__(self):
